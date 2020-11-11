b415fa0e340f809cf052cb1aea7e233aa53c3bf3
Joel Chacón Castillo <joel.chacon@cimat.mx>
2020-11-10 21:18:26 -0600

    ..

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Nuevo_Paradigma
-version1 se considera el fitness de cada individuo con el indicador R2 y el operador de cruce se mantiene igual. No se pueden comparar adecuadamente los individuos pues las soluciones dominadas no tienen contribución.
-version2 igual que la version1 pero se considera un operador de cruce juntanto al target y al trial.
-version3 igual que en la version2 pero se considera diversidad.
-version4 se construye cada individuo considerando la diversidad de juntar todos los individuos.
 
---
+++ src/version4/global.h
@@ -0,0 +1,52 @@
+#ifndef __GLOBAL_H_
+#define __GLOBAL_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <memory.h>
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#include "Kuhn_Munkres.h"
+
+using namespace std;
+
+#include "random.h"
+
+
+//------------- Parameters in test instance ------------------
+
+int     nvar,  nobj;                    //  the number of variables and objectives
+int param_k, param_l;
+
+double  vlowBound[3000] ,   vuppBound[3000];   //  lower and upper bounds of variables
+
+char    strTestInstance[256];
+char    strpath[800];
+
+
+//------------- Parameters in random number ------------------
+int     seed    = 177;
+long    rnd_uni_init;        
+
+
+//------------- Parameters in MOEA/D -------------------------
+
+vector <double> idealpoint;
+double          scale[100];  
+
+
+int		etax    = 20, 	etam    = 50;   // distribution indexes of crossover and mutation
+
+double  realx,  realm,  realb = 0.9;    // crossover, mutation, selection probabilities
+
+double Di, Df, CR, F; // distance available of the hypersphere...
+int nInd, nPop, nOffspring, nWeight;
+long long max_nfes;
+
+#endif
