2a8f8d0c25bb9c645427374848d794c1189cd8ef
Joel Chacón Castillo <joel.chacon@cimat.mx>
2020-12-18 01:18:01 -0600

    ..

---
+++ src/test/global.h
@@ -0,0 +1,57 @@
+#ifndef __GLOBAL_H_
+#define __GLOBAL_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <memory.h>
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#define MAX_VAR 2000
+#include "Kuhn_Munkres.h"
+
+using namespace std;
+
+#include "random.h"
+
+#define rnd_uni (min((rand()/double(RAND_MAX)), 1.0-1.0e-130))
+//------------- Parameters in test instance ------------------
+
+int     nvar,  nobj;                    //  the number of variables and objectives
+int param_k, param_l;
+
+double  vlowBound[MAX_VAR] ,   vuppBound[MAX_VAR];   //  lower and upper bounds of variables
+
+char    strTestInstance[256];
+char    strpath[800];
+
+
+//------------- Parameters in MOEA/D -------------------------
+
+vector <double> idealpoint;
+int		etax    = 20, 	etam    = 50;   // distribution indexes of crossover and mutation
+double  realx,  realm,  realb = 0.9;    // crossover, mutation, selection probabilities
+
+double Di, Df, CR, F; // distance available of the hypersphere...
+int nInd, nPop, nOffspring, nWeight;
+long long max_nfes;
+
+////Hungarian information
+double cost_1[MAX_VAR][MAX_VAR], cost_2[MAX_VAR][MAX_VAR], cost_3[MAX_VAR][MAX_VAR];
+int asg_1[MAX_VAR], asg_2[MAX_VAR], asg_3[MAX_VAR];
+////
+double distance_obj(vector<double> &a, vector<double> &b)
+{
+   double dist =0.0;
+   for(int i = 0; i < a.size(); i++)
+     dist += ((a[i]-b[i])*(a[i]-b[i]));
+   return dist;
+}
+
+long    rnd_uni_init;        
+#endif
---
+++ src/test/recomb.h
@@ -0,0 +1,120 @@
+#ifndef _RECOMBINATION_H_
+#define _RECOMBINATION_H_
+
+#include "global.h"
+#include "individual.h"
+
+/* Routine for real polynomial mutation of an T */
+void realmutation(vector<double> &ind, double rate)
+{
+    long double rnd, delta1, delta2, mut_pow, deltaq;
+    long double y, yl, yu, val, xy;
+    long double eta_m = etam;
+
+	int id_rnd = rand()%nvar;
+    for (int j=0; j<nvar; j++)
+    {
+        if (rnd_uni<=rate)
+        {
+            y  = ind[j];
+            yl = vlowBound[j];
+            yu = vuppBound[j];
+            delta1 = (y-yl)/(yu-yl);
+            delta2 = (yu-y)/(yu-yl);
+            //rnd = rnd_uni;
+            rnd = rnd_uni2(&rnd_uni_init);
+            mut_pow = 1.0/(eta_m+1.0);
+            if (rnd <= 0.5)
+            {
+                xy = 1.0-delta1;
+                val = 2.0*rnd+(1.0-2.0*rnd)*(pow(xy,(eta_m+1.0)));
+                deltaq =  pow(val,mut_pow) - 1.0;
+            }
+            else
+            {
+                xy = 1.0-delta2;
+                val = 2.0*(1.0-rnd)+2.0*(rnd-0.5)*(pow(xy,(eta_m+1.0)));
+                deltaq = 1.0 - (pow(val,mut_pow));
+            }
+            y = y + deltaq*(yu-yl);
+            if (y<yl)
+                y = yl;
+            if (y>yu)
+                y = yu;
+            ind[j] = y;
+        }
+    }
+    return;
+}
+void diff_evo_xoverA(CIndividual &ind0, CIndividual &ind1, CIndividual &ind2, CIndividual &ind3, CIndividual &child, double CR, double F, vector<bool> &changed)
+{
+	child.x_var= ind0.x_var;
+	child.y_obj= ind0.y_obj;
+	// Check Whether the cross-over to be performed
+	/*Loop over no of variables*/
+	int idx_rnd = rand()%(nvar*nInd);//int(rnd_uni(&rnd_uni_init)*nvar*nInd);
+        for(int i = 0; i < nInd; i++)
+        {
+          for(int j = 0; j < nInd; j++)
+          {
+	         cost_1[i][j] = -distance_obj(ind0.y_obj[i], ind1.y_obj[j]);
+	         cost_2[i][j] = -distance_obj(ind0.y_obj[i], ind2.y_obj[j]);
+	         cost_3[i][j] = -distance_obj(ind0.y_obj[i], ind3.y_obj[j]);
+          }
+        } 
+        hungarian(cost_1, asg_1);
+        hungarian(cost_2, asg_2);
+        hungarian(cost_3, asg_3);
+	for(int n=0;n<nvar*nInd; n++)
+	{
+	  double rnd = rnd_uni;
+	  if(rnd<CR||n==idx_rnd)
+	  {
+              child.x_var[n/nvar][n%nvar] = ind1.x_var[asg_1[n/nvar]][n%nvar] + F*(ind2.x_var[asg_2[n/nvar]][n%nvar] - ind3.x_var[asg_3[n/nvar]][n%nvar]);
+	      if(child.x_var[n/nvar][n%nvar]<vlowBound[n%nvar])
+ 	           child.x_var[n/nvar][n%nvar] = ind0.x_var[n/nvar][n%nvar];//vlowBound[n] + rnd*(ind0.x_var[n] - vlowBound[n]);
+	      if(child.x_var[n/nvar][n%nvar]>vuppBound[n%nvar])
+	            child.x_var[n/nvar][n%nvar] = ind0.x_var[n/nvar][n%nvar];//vuppBound[n] - rnd*(vuppBound[n] - ind0.x_var[n]);
+	      if(child.x_var[n/nvar][n%nvar]<vlowBound[n%nvar]) child.x_var[n/nvar][n%nvar] = vlowBound[n%nvar];
+	      if(child.x_var[n/nvar][n%nvar]>vuppBound[n%nvar]) child.x_var[n/nvar][n%nvar] = vuppBound[n%nvar];
+	       changed[n/nvar]=true;
+	  }
+	}
+}
+void diff_evo_xoverA_exp(CIndividual &ind0, CIndividual &ind1, CIndividual &ind2, CIndividual &ind3, CIndividual &child, double CR, double F, vector<bool> &changed)
+{
+	// Check Whether the cross-over to be performed
+	/*Loop over no of variables*/
+	int n = rand()%(nvar*nInd);//int(rnd_uni(&rnd_uni_init)*nvar*nInd);
+	child.x_var= ind0.x_var;
+	child.y_obj= ind0.y_obj;
+        for(int i = 0; i < nInd; i++)
+        {
+          for(int j = 0; j < nInd; j++)
+          {
+	         cost_1[i][j] = -distance_obj(ind0.y_obj[i], ind1.y_obj[j]);
+	         cost_2[i][j] = -distance_obj(ind0.y_obj[i], ind2.y_obj[j]);
+	         cost_3[i][j] = -distance_obj(ind0.y_obj[i], ind3.y_obj[j]);
+          }
+        } 
+        hungarian(cost_1, asg_1);
+        hungarian(cost_2, asg_2);
+        hungarian(cost_3, asg_3);
+
+	int cont =0;
+	do{
+         child.x_var[n/nvar][n%nvar] = ind1.x_var[asg_1[n/nvar]][n%nvar] + F*(ind2.x_var[asg_2[n/nvar]][n%nvar] - ind3.x_var[asg_3[n/nvar]][n%nvar]);
+	  if(child.x_var[n/nvar][n%nvar]<vlowBound[n%nvar])
+ 	       child.x_var[n/nvar][n%nvar] = ind0.x_var[n/nvar][n%nvar];//vlowBound[n] + rnd*(ind0.x_var[n] - vlowBound[n]);
+	  if(child.x_var[n/nvar][n%nvar]>vuppBound[n%nvar])
+	        child.x_var[n/nvar][n%nvar] = ind0.x_var[n/nvar][n%nvar];//vuppBound[n] - rnd*(vuppBound[n] - ind0.x_var[n]);
+	  if(child.x_var[n/nvar][n%nvar]<vlowBound[n%nvar]) child.x_var[n/nvar][n%nvar] = vlowBound[n%nvar];
+	  if(child.x_var[n/nvar][n%nvar]>vuppBound[n%nvar]) child.x_var[n/nvar][n%nvar] = vuppBound[n%nvar];
+	   changed[n/nvar]=true;
+	   n++;
+	   n %= (nvar*nInd);
+	   cont++;
+	}
+        while(rnd_uni < CR && cont < (nvar*nInd) );
+}
+#endif
